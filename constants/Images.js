cfa4bfb70b2c149892cc096ac2bc96f356c92aaa
tran thanh tu <tttuab@gmail.com>
2023-03-05 21:37:15 +0700

    update

--- constants/Images.js
+++ constants/Images.js
@@ -1,4 +1,7 @@
 export default {
-    LOGO_TLS: require("../images/logos/Logo-Thuy_Loi.png"),
-
+  LOGO_TLS: require("../images/logos/Logo-Thuy_Loi.png"),
+  BANNER1: require("../images/banners/banner_one.jpg"),
+  BANNER2: require("../images/banners/banner_two.jpg"),
+  BANNER3: require("../images/banners//banner_three.jpg"),
+  BANNER4: require("../images/banners/banner_four.jpg"),
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,28 @@
 //tran tu
-import { StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import React from "react";
 import Colors from "../constants/Colors";
 import Feather from "react-native-vector-icons/Feather";
+import Swiper from "react-native-swiper";
+import Images from "../constants/Images";
+
+const renderPagination = (index, total, context) => {
+  return (
+    <View style={styles.paginationStyle}>
+      <Text style={{ color: "grey" }}>
+        <Text style={styles.paginationText}>{index + 1}</Text>/{total}
+      </Text>
+    </View>
+  );
+};
 
 const HomeScreen = () => {
   return (
@@ -40,7 +60,44 @@ const HomeScreen = () => {
         />
       </View>
 
-      <Text>HomeScreen</Text>
+      <View style={{ flex: 1, width: "100%", height: 80 }}>
+        <Swiper
+          containerStyle={styles.wrapper}
+          showsButtons={false}
+          loop={true}
+          autoplay={true}
+          paginationStyle={false}
+          showsPagination={false}
+        >
+          <View style={styles.slide}>
+            <Image
+              style={styles.image}
+              source={require("../images/banners/banner_one.jpg")}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              style={styles.image}
+              source={require("../images/banners/banner_two.jpg")}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              style={styles.image}
+              source={require("../images/banners/banner_three.jpg")}
+            />
+          </View>
+          <View style={styles.slide}>
+            <Image
+              style={styles.image}
+              source={require("../images/banners/banner_four.jpg")}
+            />
+          </View>
+        </Swiper>
+      </View>
+      <View style={{ width: "100%", height: 510 }}>
+        <Text>Home</Text>
+      </View>
     </View>
   );
 };
@@ -90,5 +147,30 @@ const styles = StyleSheet.create({
     width: 100,
     color: Colors.DEFAULT_BLACK,
     flex: 1,
+  },
+  wrapper: { width: "100%", height: 80 },
+  slide: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  paginationStyle: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  paginationText: {
+    color: "white",
+    fontSize: 20,
   },
 });
